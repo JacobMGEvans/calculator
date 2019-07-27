78ef61f3b1dc05355b1131a7bb125566d23ae8e6
Jacob Evans <cloud887@gmail.com>
2019-07-27 13:45:03 -0500

    tailwind

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import Express from 'express';
 
 const PORT = process.env.PORT || 1234;
 
-express()
+Express()
   .use(Express.static(`${__dirname}/dist`))
   .get(`*`, (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
   .listen(PORT, () => console.log(`__SERVER_RUNNING__`, PORT));
