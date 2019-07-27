277f1574c709db2e80c8f4fa67d7364538e80bc7
Jacob Evans <cloud887@gmail.com>
2019-07-27 14:27:25 -0500

    CSS fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './components/calculator';
-import './index.css';
+import './style.css';
 
 ReactDOM.render(<Calculator />, document.getElementById('root'));
