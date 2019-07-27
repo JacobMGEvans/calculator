d0a6856ddf32bc747bf77d3be265a95cce7b4aee
Jacob Evans <cloud887@gmail.com>
2019-07-27 13:21:08 -0500

    removed point

--- components/calculator-key.js
+++ components/calculator-key.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PointTarget from 'react-point';
 
 const CalculatorKey = ({ onPress, className, ...props }) => (
-  <PointTarget onPoint={onPress}>
-    <button className={`calculator-key ${className}`} {...props} />
-  </PointTarget>
+  <button
+    onClick={onPress}
+    className={`calculator-key ${className}`}
+    {...props}
+  />
 );
 
 export default CalculatorKey;
