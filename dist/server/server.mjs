d97f0ddd2f39e88ac304677988cc45276ba9cb43
Jacob Evans <jacobevans@jacobs-mbp.lan>
2023-09-30 15:59:12 -0500

    Vite & TS

--- dist/server/server.mjs
+++ dist/server/server.mjs
@@ -1,18 +1,12 @@
 import fastify from "fastify";
 import fastifyStatic from "@fastify/static";
 import path from "node:path";
-
 const server = fastify();
-
 const PORT = process.env.PORT || 1234;
-
-// Serve static files from the `dist` directory
 server.register(fastifyStatic, {
-  root: path.join(__dirname, "dist"),
+  root: path.join(__dirname, "dist")
 });
-
 server.get("*", (_, reply) => {
   reply.send(path.join(__dirname, "dist/index.html"));
 });
-
 server.listen(() => console.log(`__SERVER_RUNNING__`, PORT));
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { join, dirname } from "node:path";
 import { URL, fileURLToPath } from "node:url";
 
 export default {
-  root: join(dirname(fileURLToPath(new URL(import.meta.url))), "client"),
+  root: join(dirname(fileURLToPath(new URL(import.meta.url)))),
   plugins: [],
 };
