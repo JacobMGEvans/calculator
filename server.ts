5412f15bd334eb5b9c54c1d780ff97f7107bd2e9
Jacob Evans <jacobevans@jacobs-mbp.lan>
2023-09-30 16:18:17 -0500

    I am starting to understand these frontend tools again

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
 import fastify from "fastify";
 import fastifyStatic from "@fastify/static";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const server = fastify();
 
 const PORT = process.env.PORT || 1234;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve static files from the `dist` directory
 server.register(fastifyStatic, {
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { join, dirname } from "node:path";
 import { URL, fileURLToPath } from "node:url";
 
 export default {
-  root: join(dirname(fileURLToPath(new URL(import.meta.url)))),
-  plugins: [],
+  root: join(dirname(fileURLToPath(new URL(import.meta.url)))), // This is the default
 };
